587d6a8f0273e3f824cd6a64cdf6058c5e89e5fc
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-05 16:05:55 -0500

    started the unpack() test

--- src/ethernet-test.c
+++ src/ethernet-test.c
@@ -68,8 +68,56 @@ static void test_pack(void)
 	assert(bufdata[14 + 64 + 3] == 21);
 }
 
+static void test_unpack(void)
+{
+	struct libnet_ethernet ethernet;
+
+	libnet_ethernet_init(&ethernet);
+
+	char bufdata[128];
+	// set destination
+	bufdata[0] = 0x23;
+	bufdata[1] = 0x32;
+	bufdata[2] = 0x23;
+	bufdata[3] = 0x32;
+	bufdata[4] = 0x23;
+	bufdata[5] = 0x32;
+	// set source
+	bufdata[6] = 0x12;
+	bufdata[7] = 0x21;
+	bufdata[8] = 0x12;
+	bufdata[9] = 0x21;
+	bufdata[10] = 0x12;
+	bufdata[11] = 0x21;
+	// set length to two
+	bufdata[12] = 0x00;
+	bufdata[13] = 0x02;
+	// set payload
+	bufdata[14] = 't';
+	bufdata[15] = 'e';
+	bufdata[16] = 's';
+	bufdata[17] = 't';
+	// set CRC32
+	bufdata[18] = 0;
+	bufdata[19] = 0;
+	bufdata[20] = 0;
+	bufdata[21] = 0;
+
+	struct libnet_buffer buffer;
+	libnet_buffer_init(&buffer);
+	buffer.data = bufdata;
+	buffer.size = 22;
+	buffer.reserved = sizeof(bufdata);
+
+	int err = libnet_ethernet_unpack(&ethernet, &buffer);
+	assert(err == 0);
+	assert(memcmp(ethernet.destination.octets, "\x23\x32\x23\x32\x23\x32", 6) == 0);
+	assert(memcmp(ethernet.source.octets, "\x12\x32\x12\x21\x12\x21", 6) == 0);
+}
+
 int main(void)
 {
 	test_pack();
+	test_unpack();
 	return EXIT_SUCCESS;
 }
