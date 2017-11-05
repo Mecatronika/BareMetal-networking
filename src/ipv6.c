331d9be32c8d5b62e5c76cd3c1323ea367b5b621
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-04 22:01:38 -0400

    implemented (partially) ipv6 parsing

--- src/ipv6.c
+++ src/ipv6.c
@@ -15,8 +15,37 @@ void libnet_ipv6_address_init(struct libnet_ipv6_address *address)
 int libnet_ipv6_address_parse(struct libnet_ipv6_address *address,
                               const char *str, size_t str_size)
 {
-	(void) address;
-	(void) str;
-	(void) str_size;
-	return -1;
+	size_t quartet_index = 0;
+
+	// TODO : handle '::'
+
+	for (size_t i = 0; i < str_size; i++)
+	{
+		unsigned char value = 0;
+
+		char c = str[i];
+		if (c == ':')
+			continue;
+
+		if ((c >= 'A') && (c <= 'F'))
+			value = (unsigned char)(c - 'A') + 10;
+		else if ((c >= 'a') && (c <= 'f'))
+			value = (unsigned char)(c - 'a') + 10;
+		else if ((c >= '0') && (c <= '9'))
+			value = (unsigned char)(c - '0');
+		else
+			// malformed string
+			return -1;
+
+		if ((quartet_index % 2) == 0)
+			value <<= 4;
+
+		size_t octet_index = quartet_index / 2;
+
+		address->octets[octet_index] |= value;
+
+		quartet_index++;
+	}
+
+	return 0;
 }
