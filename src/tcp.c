f285eb7c71271b2c57be6c03f7e8688aa2716c5a
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-05 11:13:33 -0500

    implemented port parsing

--- src/tcp.c
+++ src/tcp.c
@@ -6,6 +6,49 @@
 
 #include <libnet/tcp.h>
 
+#include <limits.h>
+
+static int parse_port(unsigned int *port_ptr,
+                      const char *str, size_t str_size)
+{
+	// using an unsigned long int ensures
+	// that if a number greater than 65535
+	// is passed, we can detect that it is
+	// greater than the 16-bit max (unsigned
+	// int, on some platforms, may only be
+	// 16-bits).
+	unsigned long int port = 0;
+
+	if (str_size > 5)
+	{
+		// 65535 is the max, so
+		// if more than five digits
+		// have been passed, only five
+		// will be used.
+		str_size = 5;
+	}
+
+	for (size_t i = 0; i < str_size; i++)
+	{
+		char c = str[i];
+		if ((c >= '0') && (c <= '9'))
+		{
+			port *= 10;
+			port += c - '0';
+		}
+		else
+			return -1;
+	}
+
+	// port exceeds maximum
+	if (port > 65535)
+		return -1;
+
+	*port_ptr = (unsigned int) port;
+
+	return 0;
+}
+
 void libnet_tcp_init(struct libnet_tcp *tcp)
 {
 	tcp->source = 0;
@@ -23,18 +66,12 @@ int libnet_tcp_set_source(struct libnet_tcp *tcp,
                           const char *str,
                           size_t str_size)
 {
-	(void) tcp;
-	(void) str;
-	(void) str_size;
-	return -1;
+	return parse_port(&tcp->source, str, str_size);
 }
 
 int libnet_tcp_set_destination(struct libnet_tcp *tcp,
                                const char *str,
                                size_t str_size)
 {
-	(void) tcp;
-	(void) str;
-	(void) str_size;
-	return -1;
+	return parse_port(&tcp->destination, str, str_size);
 }
