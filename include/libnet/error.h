49b029910db99a43196574900d1666cadbbcd709
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-06 18:37:06 -0500

    assigning numbers to enumeration fields

--- include/libnet/error.h
+++ include/libnet/error.h
@@ -15,16 +15,19 @@ extern "C"
 #endif
 
 enum {
-	/// @brief No error has occured
+	/// @brief No error has occured.
 	LIBNET_ERROR_NONE = 0,
 	/// @brief A protocol handler found a
 	/// bad field.
-	LIBNET_ERROR_BAD_FIELD,
+	LIBNET_ERROR_BAD_FIELD = 1,
 	/// @brief A protocol handler needs more
 	/// data to parse a header (or footer)
-	LIBNET_ERROR_MISSING_DATA,
-	/// @brief Unknown error has occured
-	LIBNET_ERROR_UNKNOWN
+	LIBNET_ERROR_MISSING_DATA = 2,
+	/// @brief Unknown error has occured.
+	/// This field is set to -1 for backwards
+	/// compatibility. Functions may continue
+	/// to return -1 if an unknown error occured.
+	LIBNET_ERROR_UNKNOWN = -1
 };
 
 #ifdef __cplusplus
