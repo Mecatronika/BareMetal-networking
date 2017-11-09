db515b740f1dff41e51db3d6c5f0463b4f1acd15
Taylor Holberton <taylorcholberton@gmail.com>
2017-11-08 21:22:24 -0500

    updated examples dir

--- examples/example1.c
+++ examples/example1.c
@@ -1,20 +1,20 @@
-// =========================================================
-// libnet -- A network stack implementation for BareMetal OS
+//============================================================
+// netstack -- A network stack implementation for BareMetal OS
 //
 // Copyright (C) 2017 Return Infinity -- see LICENSE
-// =========================================================
+//============================================================
 
-#include <libnet/stack.h>
+#include <netstack/stack.h>
 
 #include <stdlib.h>
 
 int main(void)
 {
-	struct libnet_stack stack;
+	struct netstack_stack stack;
 
-	libnet_stack_init(&stack);
+	netstack_stack_init(&stack);
 
-	libnet_stack_done(&stack);
+	netstack_stack_done(&stack);
 
 	return EXIT_SUCCESS;
 }
--- examples/tcp-pack.c
+++ examples/tcp-pack.c
@@ -1,27 +1,27 @@
-// =========================================================
-// libnet -- A network stack implementation for BareMetal OS
+//============================================================
+// netstack -- A network stack implementation for BareMetal OS
 //
 // Copyright (C) 2017 Return Infinity -- see LICENSE
-// =========================================================
+//============================================================
 
-#include <libnet/buffer.h>
-#include <libnet/mutator.h>
-#include <libnet/stack.h>
+#include <netstack/buffer.h>
+#include <netstack/mutator.h>
+#include <netstack/stack.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static int mutate_ethernet(const void *mutator_data,
-                           struct libnet_ethernet *ethernet)
+                           struct netstack_ethernet *ethernet)
 {
 	(void) mutator_data;
 
 	ethernet->type = LIBNET_ETHERTYPE_IPV6;
 
 	int err = 0;
-	err |= libnet_ethernet_set_source(ethernet, "00:11:22:33:44:55", 17);
-	err |= libnet_ethernet_set_destination(ethernet, "33:44:55:66:77:88", 17);
+	err |= netstack_ethernet_set_source(ethernet, "00:11:22:33:44:55", 17);
+	err |= netstack_ethernet_set_destination(ethernet, "33:44:55:66:77:88", 17);
 	return err;
 }
 
@@ -30,28 +30,28 @@ const char ipv6_source[] = "2001:0000:3238:DFE1:0063:0000:0000:FEFB";
 const char ipv6_destination[] = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
 
 static int mutate_ipv6(const void *mutator_data,
-                       struct libnet_ipv6 *ipv6)
+                       struct netstack_ipv6 *ipv6)
 {
 	(void) mutator_data;
 
 	int err = 0;
-	err |= libnet_ipv6_set_source(ipv6, ipv6_source, sizeof(ipv6_source) - 1);
-	err |= libnet_ipv6_set_destination(ipv6, ipv6_destination, sizeof(ipv6_destination) - 1);
+	err |= netstack_ipv6_set_source(ipv6, ipv6_source, sizeof(ipv6_source) - 1);
+	err |= netstack_ipv6_set_destination(ipv6, ipv6_destination, sizeof(ipv6_destination) - 1);
 	return err;
 }
 
 static int mutate_tcp(const void *mutator_data,
-                      struct libnet_tcp *tcp)
+                      struct netstack_tcp *tcp)
 {
 	(void) mutator_data;
 
 	int err = 0;
-	err |= libnet_tcp_set_source(tcp, "80", 2);
-	err |= libnet_tcp_set_destination(tcp, "80", 2);
+	err |= netstack_tcp_set_source(tcp, "80", 2);
+	err |= netstack_tcp_set_destination(tcp, "80", 2);
 	return err;
 }
 
-static int export_buffer(const struct libnet_buffer *buffer)
+static int export_buffer(const struct netstack_buffer *buffer)
 {
 	FILE *file = fopen("tcp-packet.pcap", "wb");
 	if (file == NULL)
@@ -102,32 +102,32 @@ const char http_response[] = "HTTP/1.1 200 OK\n"
 
 int main(void)
 {
-	struct libnet_stack stack;
+	struct netstack_stack stack;
 
-	libnet_stack_init(&stack);
+	netstack_stack_init(&stack);
 
 	int err = 0;
-	err |= libnet_stack_push_ethernet(&stack);
-	err |= libnet_stack_push_ipv6(&stack);
-	err |= libnet_stack_push_tcp(&stack);
+	err |= netstack_stack_push_ethernet(&stack);
+	err |= netstack_stack_push_ipv6(&stack);
+	err |= netstack_stack_push_tcp(&stack);
 	if (err != 0)
 	{
 		fprintf(stderr, "Failed to construct network stack.\n");
-		libnet_stack_done(&stack);
+		netstack_stack_done(&stack);
 		return EXIT_FAILURE;
 	}
 
-	struct libnet_mutator mutator;
-	libnet_mutator_init(&mutator);
+	struct netstack_mutator mutator;
+	netstack_mutator_init(&mutator);
 	mutator.mutate_ethernet = mutate_ethernet;
 	mutator.mutate_ipv6 = mutate_ipv6;
 	mutator.mutate_tcp = mutate_tcp;
 
-	err = libnet_stack_mutate(&stack, &mutator);
+	err = netstack_stack_mutate(&stack, &mutator);
 	if (err != 0)
 	{
 		fprintf(stderr, "Failed to mutate network stack.\n");
-		libnet_stack_done(&stack);
+		netstack_stack_done(&stack);
 		return EXIT_FAILURE;
 	}
 
@@ -135,22 +135,22 @@ int main(void)
 	memset(msgbuf, 0, sizeof(msgbuf));
 	strcat(msgbuf, http_response);
 
-	struct libnet_buffer buffer;
+	struct netstack_buffer buffer;
 	buffer.data = msgbuf;
 	buffer.size = sizeof(http_response);
 	buffer.reserved = sizeof(msgbuf);
 
-	err = libnet_stack_pack(&stack, &buffer);
+	err = netstack_stack_pack(&stack, &buffer);
 	if (err != 0)
 	{
 		fprintf(stderr, "Failed to pack message.\n");
-		libnet_stack_done(&stack);
+		netstack_stack_done(&stack);
 		return EXIT_FAILURE;
 	}
 
 	export_buffer(&buffer);
 
-	libnet_stack_done(&stack);
+	netstack_stack_done(&stack);
 
 	return EXIT_SUCCESS;
 }
